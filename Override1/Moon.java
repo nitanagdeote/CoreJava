e990b9b88e564291f7ab2ea7c52e7b4e21bae07f
Nita Nagdewate <nitanagdeote1@gmail.com>
2015-09-12 20:07:18 +0530

    Update Moon.java

--- Override1/Moon.java
+++ Override1/Moon.java
@@ -3,18 +3,15 @@ package package2;
 import package1.Planet;
 
 public class Moon extends Planet{
-
 	public void PlanetName() {
-	  System.out.println("Moon is a planet.")  ;
+	 System.out.println("Moon is a planet.")  ;
 	}
 	
 	
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-    Moon m = new Moon() ;
-		m.PlanetName() ;
-	
+	Moon m = new Moon() ;
+	m.PlanetName() ;
 	}
 
 }
